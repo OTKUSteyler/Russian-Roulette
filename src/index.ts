36ec62ff00d2a9836a9634b4406fb7e194dba221
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-05 21:20:45 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
-import { storage } from "@vendetta/plugin";
+import { registerSettings } from "@vendetta/settings";
 import settings from "./Settings";
 
-// Function to get installed plugins
-const getInstalledPlugins = () => Object.keys(vendetta.plugins.plugins);
-
-// Russian Roulette Logic
-const playRoulette = () => {
-    const plugins = getInstalledPlugins();
-    if (plugins.length === 0) {
-        console.log("[Roulette] No plugins to remove.");
-        return "No plugins installed.";
-    }
-
-    // Roll a number
-    const roll = Math.floor(Math.random() * plugins.length);
-    const chosenPlugin = plugins[roll];
-
-    // Delete the chosen plugin
-    vendetta.plugins.uninstallPlugin(chosenPlugin);
-    console.log(`[Roulette] Removed plugin: ${chosenPlugin}`);
-    return `Removed plugin: ${chosenPlugin}`;
-};
-
 export const onLoad = () => {
-    console.log("Russian Roulette Plugin Loaded!");
+    registerSettings("Russian Roulette", settings);
 };
 
 export const onUnload = () => {
-    console.log("Russian Roulette Plugin Unloaded!");
+    registerSettings("Russian Roulette", null);
 };
-
-export { settings, playRoulette };
