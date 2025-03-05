8bd0c6f1bfd8d02972ea180320c9e200998d5eb6
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-05 21:30:24 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import { registerSettings } from "@vendetta/settings";
+import { showToast } from "@vendetta/ui/toasts";
+import { uninstallPlugin, plugins } from "@vendetta/plugins";
 import settings from "./Settings";
+
+export const playRoulette = () => {
+    const pluginList = Object.keys(plugins);
+    
+    if (pluginList.length === 0) {
+        showToast("No plugins installed to remove.", { type: "info" });
+        return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * pluginList.length);
+    const selectedPlugin = pluginList[randomIndex];
+
+    uninstallPlugin(selectedPlugin);
+    showToast(`Plugin "${selectedPlugin}" has been removed.`, { type: "danger" });
+};
 
 export const onLoad = () => {
     registerSettings("Russian Roulette", settings);
